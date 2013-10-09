846d0b8d3b260e46dceaa52a2818fd80642b7170
alramlo <alramlo@inf.upv.es>
2013-10-09 21:17:05 +0100

    Daos

--- automaticDiet/src/persistencia/AbstractDAO.java
+++ automaticDiet/src/persistencia/AbstractDAO.java
@@ -1,5 +1,16 @@
 package persistencia;
 
+import javax.persistence.*;
+
 public abstract class AbstractDAO {
+	
+	EntityManager entityManager = null;
+	
+	public AbstractDAO (){
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("automaticDiet");
+		this.entityManager = emf.createEntityManager();
+		
+	}
 
 }
