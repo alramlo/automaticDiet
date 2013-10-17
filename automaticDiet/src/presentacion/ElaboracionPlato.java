70402de55a6cd8a02cfe40bdb40b710263157582
SERGIO <sercama5@inf.upv.es>
2013-10-17 18:58:32 +0200

    wu elaboracion de plato

--- automaticDiet/src/presentacion/ElaboracionPlato.java
+++ automaticDiet/src/presentacion/ElaboracionPlato.java
@@ -1,6 +1,5 @@
 package presentacion;
 
-import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -17,7 +16,6 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.JTextPane;
 
 import modelo.Plato;
@@ -30,15 +28,22 @@ public class ElaboracionPlato extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = -35301839624147280L;
-	private JTextField textFieldBuscar;
 	private JComboBox<String> comboBox;
 	private JTextPane textPaneElaboracion;
 	private JLabel imagenPlato;
+	private static Controlador control;
 
 	/**
 	 * Create the panel.
 	 */
 	public ElaboracionPlato(Object platoVuelta) {
+		
+		try {
+			control=Controlador.dameControlador();
+		} catch (DominioExcepcion e3) {
+			// TODO Auto-generated catch block
+			e3.printStackTrace();
+		}
 		
 		setLayout(null);
 		
@@ -52,26 +57,50 @@ public class ElaboracionPlato extends JPanel {
 		buttonPanelIngredientes.setBounds(366, 39, 186, 39);
 		add(buttonPanelIngredientes);
 		
-		textFieldBuscar = new JTextField();
-		textFieldBuscar.setBounds(10, 110, 256, 29);
-		add(textFieldBuscar);
-		textFieldBuscar.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("Buscar plato:");
-		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 16));
-		lblNewLabel.setBounds(10, 79, 256, 20);
-		add(lblNewLabel);
-		
-		
-		comboBox = new JComboBox<String>();
-		
+		comboBox = new JComboBox<String>(/*control.todosPlatos()*/);	
 		//Extraer todos los platos del usuario
 		
 		comboBox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Plato plato = new Plato();
-				plato.setNombre(comboBox.getSelectedItem()+"");
-				//Controlador
+				Plato platoVuelta;
+				try {
+					platoVuelta = control.consultarPlato(comboBox.getSelectedItem()+"");
+				if(platoVuelta!=null){
+					textPaneElaboracion.setText(platoVuelta.getElaboracion());
+					String[] vector = new String[1];
+					vector[0]=((Plato)platoVuelta).getNombre();
+					comboBox=new JComboBox<String>(vector);
+					File file=null;
+					try {
+						file = new File("automaticDiet");
+						FileOutputStream fos = new FileOutputStream (file);
+						fos.write(((Plato) platoVuelta).getImagen());
+						fos.close();
+					} catch (FileNotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					ImageIcon im=null;
+	        		BufferedImage buffer;
+					try {
+						buffer = ImageIO.read(file);
+						im = new ImageIcon(buffer);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+	        		Image image=im.getImage();
+	        		Image newImage = image.getScaledInstance(346, 325, java.awt.Image.SCALE_SMOOTH);
+	        		imagenPlato.setIcon(new ImageIcon(newImage));
+				}
+				} catch (DAOExcepcion e2) {
+					// TODO Auto-generated catch block
+					e2.printStackTrace();
+				}
 			}
 		});
 		comboBox.setBounds(366, 223, 424, 30);
@@ -100,57 +129,5 @@ public class ElaboracionPlato extends JPanel {
 		lblNewLabel_3.setBounds(366, 143, 424, 29);
 		add(lblNewLabel_3);
 		
-		JButton buttonBuscar = new JButton("Buscar");
-		buttonBuscar.setFont(new Font("Arial", Font.PLAIN, 14));
-		buttonBuscar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				try {
-					Plato platoVuelta = Controlador.dameControlador().consultarPlato(textFieldBuscar.getText());
-					if(platoVuelta!=null){
-						textFieldBuscar.setText(platoVuelta.getNombre());
-						textPaneElaboracion.setText(platoVuelta.getElaboracion());
-						String[] vector = new String[1];
-						vector[0]=((Plato)platoVuelta).getNombre();
-						comboBox=new JComboBox<String>(vector);
-						File file=null;
-						try {
-							file = new File("automaticDiet");
-							FileOutputStream fos = new FileOutputStream (file);
-							fos.write(((Plato) platoVuelta).getImagen());
-							fos.close();
-						} catch (FileNotFoundException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-						catch (IOException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-						ImageIcon im=null;
-		        		BufferedImage buffer;
-						try {
-							buffer = ImageIO.read(file);
-							im = new ImageIcon(buffer);
-						} catch (IOException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-		        		Image image=im.getImage();
-		        		Image newImage = image.getScaledInstance(346, 325, java.awt.Image.SCALE_SMOOTH);
-		        		imagenPlato.setIcon(new ImageIcon(newImage));
-					}
-				} catch (DAOExcepcion e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (DominioExcepcion e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		});
-		buttonBuscar.setBackground(Color.RED);
-		buttonBuscar.setBounds(177, 148, 89, 23);
-		add(buttonBuscar);
-
 	}
 }
