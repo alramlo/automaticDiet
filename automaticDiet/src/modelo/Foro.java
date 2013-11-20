af95319bf75634926758325e0b48f421d7e9b2c5
SERGIO <sercama5@inf.upv.es>
2013-11-20 13:04:30 +0100

    Foro contiene a su creador

--- automaticDiet/src/modelo/Foro.java
+++ automaticDiet/src/modelo/Foro.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -38,6 +40,10 @@ public class Foro implements Serializable {
 
 	@Column(name="VISITAR")
 	private int visitar;
+	
+	@ManyToOne
+	@JoinColumn(name="CREADOR")
+	private Usuario creador;
 
 	public Foro() {
 	}
